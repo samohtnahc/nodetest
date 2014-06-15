4e0964dcbeda2f66589f4c373644659f2350ff73
Samoht Nahc <samohtnahc@gmail.com>
2014-06-14 19:21:09 -0700

    test comment push

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -13,6 +13,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//test
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
